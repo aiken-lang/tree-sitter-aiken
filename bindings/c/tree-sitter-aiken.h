b03a212e3099306f3323bc7d70c96d38c482ba10
rvcas <x@rvcas.dev>
2024-08-12 12:30:06 -0400

    chore: update tree-sitter-cli

---
+++ bindings/c/tree-sitter-aiken.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_AIKEN_H_
+#define TREE_SITTER_AIKEN_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_aiken(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_AIKEN_H_
---
+++ bindings/python/tree_sitter_aiken/__init__.py
@@ -0,0 +1,5 @@
+"Aiken grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
